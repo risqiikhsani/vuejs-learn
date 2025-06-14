1ec7f8ef0e64b1f5e2ed6959450a63e7a99a7297
risqiikhsani <risqiikhsani16@gmail.com>
2025-06-14 18:20:51 +0700

    tst

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <nav class="fixed top-0 w-full bg-slate-50 h-16 flex items-center gap-4 p-4">
+      <RouterLink to="/" class="p-2 border rounded-sm">Home</RouterLink>
+      <RouterLink to="/about" class="p-2 border rounded-sm">About</RouterLink>
+      <RouterLink to="/test" class="p-2 border rounded-sm">Test</RouterLink>
+    </nav>
+  </header>
+  <RouterView class="pt-16 container mx-auto" />
+</template>
---
+++ src/views/TestView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import TheTest from '../components/TheTest.vue'
+
+import { ref, onMounted } from 'vue'
+
+const name = ref('')
+const email = ref('')
+
+const clear = () => {
+  name.value = ''
+  email.value = ''
+}
+
+onMounted(() => {
+  console.log('mounted')
+})
+</script>
+
+<template>
+  <main>
+    <TheTest />
+    <input v-model="name" placeholder="Type something..." />
+    <p>You typed: {{ name }}</p>
+    <input v-model="email" placeholder="Type something..." />
+    <p>You typed: {{ email }}</p>
+    <button @click="clear">Clear</button>
+  </main>
+</template>
